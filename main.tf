f29ec4c966264bf732f331aa9c969b4af131368d
Kevin Allen <kevin11.allen@nttdata.com>
2024-07-24 17:03:23 -0400

    use default name for dce association

--- main.tf
+++ main.tf
@@ -60,7 +60,6 @@ resource "azurerm_monitor_data_collection_rule_association" "dcra_dcr" {
 }
 
 resource "azurerm_monitor_data_collection_rule_association" "dcra_dce" {
-  name                        = local.aks_cluster_name
   target_resource_id          = var.aks_cluster_id
   data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.dce.id
   description                 = "Association of data collection endpoint to AKS cluster"
